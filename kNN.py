9f4ac7b153f0fbc793b846db62328940f2aa4987
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2020-03-24 14:20:14 +0000

    After loading dating site data

--- kNN.py
+++ kNN.py
@@ -8,13 +8,29 @@ Created on Tue Mar 24 10:50:32 2020
 
 from numpy import *
 import operator
-
+from io import *
 def createDataset():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
-    print('Success createDataset()')
+    #print('Success createDataset()')
     return labels, group
-
+def fileToMatrix(filename):
+    file = open(filename)
+    numberOfLines = len(file.readlines())
+    retMatrix = zeros((numberOfLines,3))
+    classLabel = []
+    file.close()
+    file = open(filename)
+    index = 0
+    for line in file.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        retMatrix[index,:] = listFromLine[0:3]
+        classLabel.append(listFromLine[-1])
+        index = index + 1
+    print('Success fileToMatrix(filename)')
+    return retMatrix,classLabel
+    
 def classify(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     differenceMatrix = tile(inX,(dataSetSize,1)) - dataSet
@@ -30,7 +46,10 @@ def classify(inX, dataSet, labels, k):
                               key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0]
     
-labels,group = createDataset()
-print(classify([0,0],group,labels,3))
+#labels,group = createDataset()
+#print(classify([0,0],group,labels,3))
+group,labels = fileToMatrix('dataset.txt')
+print(group,labels[0:20])
+
 
 
